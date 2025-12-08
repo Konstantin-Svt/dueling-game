610c62a47a60a39f2eaf2df6d675e5f37d46a5f6
Konstantin S. <kostaho16@gmail.com>
2025-12-08 16:39:40 +0200

    Fix settings security. Update requirements.txt with python-dotenv. Add '.env' files to .gitignore

--- game_system/settings.py
+++ game_system/settings.py
@@ -11,6 +11,8 @@ https://docs.djangoproject.com/en/5.2/ref/settings/
 """
 
 from pathlib import Path
+from dotenv import load_dotenv
+import os
 
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
 BASE_DIR = Path(__file__).resolve().parent.parent
@@ -19,8 +21,8 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 # Quick-start development settings - unsuitable for production
 # See https://docs.djangoproject.com/en/5.2/howto/deployment/checklist/
 
-# SECURITY WARNING: keep the secret key used in production secret!
-SECRET_KEY = "django-insecure-2p)cm*ym+av3=mf^hrl&rihyr6ick_3g2+9#b_8t7tl_3z-)99"
+load_dotenv(BASE_DIR / ".env")
+SECRET_KEY = os.environ.get("SECRET_KEY")
 
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
